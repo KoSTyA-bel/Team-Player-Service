93b1fc01e10c6996b07d20d69846a66dcbcf6ffa
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-07-09 21:10:46 +0300

    Added team service skeleton

---
+++ src/TeamService.Grpc/Services/TeamService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TeamService.Grpc.Services;
+
+public class TeamService : Proto.TeamService.TeamServiceBase
+{
+    private readonly ILogger<TeamService> _logger;
+
+    public TeamService(ILogger<TeamService> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public override Task<Proto.CreateTeamResponse> CreateTeam(Proto.CreateTeamRequest request, ServerCallContext context)
+    {
+        return base.CreateTeam(request, context);
+    }
+
+    public override Task<Proto.GetTeamResponse> GetTeam(Proto.GetTeamRequest request, ServerCallContext context)
+    {
+        return base.GetTeam(request, context);
+    }
+
+    public override Task<Proto.UpdateTeamResponse> UpdateTeam(Proto.UpdateTeamRequest request, ServerCallContext context)
+    {
+        return base.UpdateTeam(request, context);
+    }
+
+    public override Task<Proto.DeleteTeamResponse> DeleteTeam(Proto.DeleteTeamRequest request, ServerCallContext context)
+    {
+        return base.DeleteTeam(request, context);
+    }
+}
