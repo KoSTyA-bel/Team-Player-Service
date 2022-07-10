7d78afcb1a6a61e8c057a89c0bf6ea154d45037b
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-07-10 15:45:02 +0300

    Added team service skeleton

---
+++ src/TeamService.BusinessLogic/Services/TeamService.cs
@@ -0,0 +1,39 @@
+﻿using TeamService.BusinessLogic.Entities;
+using TeamService.BusinessLogic.Interfaces;
+
+namespace TeamService.BusinessLogic.Services;
+
+public class TeamService : IService<Team>
+{
+    private readonly IRepository<Team> _repository;
+
+    public TeamService(IRepository<Team> repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    public Task<Team> Create(Team entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Team>> GetRange(int startPoint, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryGetById(Guid id, out Team entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Team> Update(Team entity)
+    {
+        throw new NotImplementedException();
+    }
+}
