9828847e6882dffffa32c32b10e9d70771265034
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-07-11 20:21:26 +0300

    Added mapping rules

--- src/TeamService.Grpc/MappingProfile.cs
+++ src/TeamService.Grpc/MappingProfile.cs
@@ -1,11 +1,20 @@
 ﻿using AutoMapper;
 using TeamService.BusinessLogic.Entities;
+using Service.Grpc;
 
 namespace TeamService.Grpc;
 
 public class MappingProfile : Profile
 {
     public MappingProfile()
-    {        
+    {
+        CreateMap<Team, GetTeamResponse>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()));
+        CreateMap<GetTeamResponse, Team>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.Parse(src.Id)));
+        CreateMap<string, Guid>()
+            .ConvertUsing(src => Guid.Parse(src));
+        CreateMap<Guid, string>()
+            .ConvertUsing(src => src.ToString());
     }
 }
