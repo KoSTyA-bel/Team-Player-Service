1ba19dd7e060a42d6e49aec99dfdaec65f76f550
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-07-11 12:55:35 +0300

    Added mapper skeleton

---
+++ src/TeamService.Grpc/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Proto;
+using TeamService.BusinessLogic.Entities;
+
+namespace TeamService.Grpc;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {        
+    }
+}
