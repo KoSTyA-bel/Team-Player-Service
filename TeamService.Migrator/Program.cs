6794087b93a66df243ead3bf9ece83ae99824719
Konstantsin Fedarakin <kostyafedorakin@gmail.com>
2022-07-19 11:37:13 +0300

    Added migrator

---
+++ TeamService.Migrator/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamService.DataAccess.Contexts;
+
+string connection = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new ArgumentNullException("Need enviroment variable CONNECTION_STRING");
+var optionsBuilder = new DbContextOptionsBuilder<TeamContext>();
+optionsBuilder.UseNpgsql(connection);
+
+var context = new TeamContext(optionsBuilder.Options);
+
+await context.Database.MigrateAsync();
+
+Console.WriteLine("Migration was succesful.");
